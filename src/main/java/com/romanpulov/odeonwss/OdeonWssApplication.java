60e39ce1257d6257d8523ff47d29e098ecf433fd
MrZeRo2000 <MrZeRo2020@outlook.com>
2022-04-27 13:07:47 +0300

    first commit

---
+++ src/main/java/com/romanpulov/odeonwss/OdeonWssApplication.java
@@ -0,0 +1,13 @@
+package com.romanpulov.odeonwss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdeonWssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdeonWssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romanpulov/odeonwss/OdeonWssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanpulov.odeonwss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdeonWssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
