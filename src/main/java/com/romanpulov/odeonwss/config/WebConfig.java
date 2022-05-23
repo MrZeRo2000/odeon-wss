5ccd8028a7fe943532223a886389c082d65e2792
MrZeRo2000 <MrZeRo2020@outlook.com>
2022-05-23 18:55:02 +0300

    fixed localdatetime representation

--- src/main/java/com/romanpulov/odeonwss/config/WebConfig.java
+++ src/main/java/com/romanpulov/odeonwss/config/WebConfig.java
@@ -2,14 +2,12 @@ package com.romanpulov.odeonwss.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * https://www.baeldung.com/spring-cors
  */
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
