955ab7290a0446b62a047f4489e128b1a7ed494b
MrZeRo2000 <MrZeRo2020@outlook.com>
2022-06-07 09:37:23 +0300

    fixed configuration and cross origin

--- src/main/java/com/romanpulov/odeonwss/controller/ArtistCategoryDetailsController.java
+++ src/main/java/com/romanpulov/odeonwss/controller/ArtistCategoryDetailsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api/artist-category-details", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ArtistCategoryDetailsController {
