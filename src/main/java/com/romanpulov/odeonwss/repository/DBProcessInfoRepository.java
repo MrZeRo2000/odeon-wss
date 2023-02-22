17844094da0ee3a267b57ce2e1be0c35bcfaae12
MrZeRo2000 <MrZeRo2020@outlook.com>
2023-02-22 18:04:29 +0200

    iss50 Implement persistent storage of processing progress results DBProcessInfo Repository and tests

---
+++ src/main/java/com/romanpulov/odeonwss/repository/DBProcessInfoRepository.java
@@ -0,0 +1,7 @@
+package com.romanpulov.odeonwss.repository;
+
+import com.romanpulov.odeonwss.entity.DBProcessInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DBProcessInfoRepository extends CrudRepository<DBProcessInfo, Long> {
+}
---
+++ src/test/java/com/romanpulov/odeonwss/repository/RepositoryDBProcessInfoTests.java
@@ -0,0 +1,55 @@
+package com.romanpulov.odeonwss.repository;
+
+import com.romanpulov.odeonwss.entity.DBProcessInfo;
+import com.romanpulov.odeonwss.service.processor.model.ProcessInfo;
+import com.romanpulov.odeonwss.service.processor.model.ProcessingStatus;
+import com.romanpulov.odeonwss.service.processor.model.ProcessorType;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class RepositoryDBProcessInfoTests {
+
+    @Autowired
+    DBProcessInfoRepository dbProcessInfoRepository;
+
+    @Test
+    @Order(1)
+    @Sql({"/schema.sql", "/data.sql"})
+    void testSave() {
+        assertThat(
+                StreamSupport.stream(dbProcessInfoRepository.findAll().spliterator(), false).count()
+        ).isEqualTo(0L);
+
+        DBProcessInfo dbProcessInfo = new DBProcessInfo();
+        dbProcessInfo.setProcessorType(ProcessorType.MP3_LOADER);
+        dbProcessInfo.setProcessingStatus(ProcessingStatus.INFO);
+        dbProcessInfo.setInsertDate(LocalDate.of(2022, 2, 12));
+
+        dbProcessInfoRepository.save(dbProcessInfo);
+
+        List<DBProcessInfo> dbProcessInfoList = StreamSupport
+                .stream(dbProcessInfoRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+        assertThat(dbProcessInfoList.size()).isEqualTo(1L);
+
+        DBProcessInfo savedDBProcessInfo = dbProcessInfoList.get(0);
+        assertThat(savedDBProcessInfo.getId()).isEqualTo(1L);
+        assertThat(savedDBProcessInfo.getProcessorType()).isEqualTo(ProcessorType.MP3_LOADER);
+        assertThat(savedDBProcessInfo.getProcessingStatus()).isEqualTo(ProcessingStatus.INFO);
+    }
+}
+
