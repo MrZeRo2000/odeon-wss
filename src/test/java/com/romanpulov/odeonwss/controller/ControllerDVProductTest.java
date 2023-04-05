b420698a569b0731ea807b4bb448fd12388b2482
MrZeRo2000 <MrZeRo2020@outlook.com>
2023-04-05 17:54:30 +0300

    iss#63 DVProducts table: controller test without categories

--- src/test/java/com/romanpulov/odeonwss/controller/ControllerDVProductTest.java
+++ src/test/java/com/romanpulov/odeonwss/controller/ControllerDVProductTest.java
@@ -1,9 +1,11 @@
 package com.romanpulov.odeonwss.controller;
 
+import com.romanpulov.odeonwss.builder.entitybuilder.EntityDVCategoryBuilder;
 import com.romanpulov.odeonwss.builder.entitybuilder.EntityDVOriginBuilder;
 import com.romanpulov.odeonwss.builder.entitybuilder.EntityDVProductBuilder;
 import com.romanpulov.odeonwss.entity.DVOrigin;
 import com.romanpulov.odeonwss.repository.ArtifactTypeRepository;
+import com.romanpulov.odeonwss.repository.DVCategoryRepository;
 import com.romanpulov.odeonwss.repository.DVOriginRepository;
 import com.romanpulov.odeonwss.repository.DVProductRepository;
 import org.hamcrest.Matchers;
@@ -19,6 +21,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -40,6 +45,9 @@ public class ControllerDVProductTest {
     private DVOriginRepository dvOriginRepository;
 
     @Autowired
+    private DVCategoryRepository dvCategoryRepository;
+
+    @Autowired
     private DVProductRepository dvProductRepository;
 
     @Test
@@ -56,18 +64,40 @@ public class ControllerDVProductTest {
                         .build()
         );
 
+        dvCategoryRepository.saveAll(
+                List.of(
+                        new EntityDVCategoryBuilder()
+                                .withName("Some category")
+                                .build(),
+                        new EntityDVCategoryBuilder()
+                                .withName("Another category")
+                                .build(),
+                        new EntityDVCategoryBuilder()
+                                .withName("Third category")
+                                .build()
+                )
+        );
+
         dvProductRepository.save(new EntityDVProductBuilder()
                 .withArtifactType(artifactTypeRepository.getWithDVMovies())
                 .withOrigin(origin)
                 .withTitle("Green")
+                .withOriginalTitle("Original green")
+                .withYear(1999L)
+                .withDescription("Green description")
+                .withNotes("Green notes")
                 .build());
         dvProductRepository.save(new EntityDVProductBuilder()
-                .withArtifactType(artifactTypeRepository.getWithDVMovies())
+                .withArtifactType(artifactTypeRepository.getWithDVAnimation())
                 .withOrigin(origin)
                 .withTitle("White")
+                .withCategories(dvCategoryRepository.findAll()
+                        .stream()
+                        .filter(c -> c.getId() == 2L || c.getId() == 3L)
+                        .collect(Collectors.toSet()))
                 .build());
         dvProductRepository.save(new EntityDVProductBuilder()
-                .withArtifactType(artifactTypeRepository.getWithDVMovies())
+                .withArtifactType(artifactTypeRepository.getWithDVAnimation())
                 .withOrigin(origin)
                 .withTitle("Brown")
                 .build());
@@ -82,30 +112,58 @@ public class ControllerDVProductTest {
                         .param("artifactTypeId", "202"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$", Matchers.hasSize(3)))
-                .andExpect(jsonPath("$[0].id", Matchers.equalTo(3)))
-                .andExpect(jsonPath("$[0].title", Matchers.containsString("Brown")))
-                .andExpect(jsonPath("$[1].id", Matchers.equalTo(1)))
-                .andExpect(jsonPath("$[1].title", Matchers.containsString("Green")))
-                .andExpect(jsonPath("$[2].id", Matchers.equalTo(2)))
-                .andExpect(jsonPath("$[2].title", Matchers.containsString("White")))
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].id", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$[0].title", Matchers.containsString("Green")))
         ;
         logger.debug("Get 202 result:" + result_202.andReturn().getResponse().getContentAsString());
 
         var result_203 = mockMvc.perform(get("/api/dvproduct/dvproducts/table-id-title")
                         .param("artifactTypeId", "203"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isEmpty());
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(jsonPath("$[0].id", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$[0].title", Matchers.containsString("Brown")))
+                .andExpect(jsonPath("$[1].id", Matchers.equalTo(2)))
+                .andExpect(jsonPath("$[1].title", Matchers.containsString("White")))
+        ;
         logger.debug("Get 203 result:" + result_203.andReturn().getResponse().getContentAsString());
+
+        var result_204 = mockMvc.perform(get("/api/dvproduct/dvproducts/table-id-title")
+                        .param("artifactTypeId", "204"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isEmpty());
+        logger.debug("Get 204 result:" + result_204.andReturn().getResponse().getContentAsString());
     }
 
     @Test
-    @Order(3)
-    @Rollback(value = false)
+    @Order(2)
     void testValidateGetAllIdTitleNotExistingFailed() throws Exception {
         mockMvc.perform(get("/api/dvproduct/dvproducts/table-id-title")
                         .param("artifactTypeId", "1024"))
                 .andExpect(status().isNotFound())
         ;
+    }
+
+    @Test
+    @Order(2)
+    void testGetTableMovies() throws Exception {
+        var result = mockMvc.perform(get("/api/dvproduct/dvproducts/table")
+                        .param("artifactTypeId", "202"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0]", Matchers.aMapWithSize(6)))
+                .andExpect(jsonPath("$[0].id", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$[0].dvOrigin", Matchers.aMapWithSize(1)))
+                .andExpect(jsonPath("$[0].dvOrigin.name", Matchers.equalTo("Product Origin")))
+                .andExpect(jsonPath("$[0].title", Matchers.equalTo("Green")))
+                .andExpect(jsonPath("$[0].originalTitle", Matchers.equalTo("Original green")))
+                .andExpect(jsonPath("$[0].year", Matchers.equalTo(1999)))
+                .andExpect(jsonPath("$[0].dvCategories").isArray())
+                .andExpect(jsonPath("$[0].dvCategories", Matchers.hasSize(0)))
+                ;
+        logger.debug("Get result:" + result.andReturn().getResponse().getContentAsString());
     }
 }
