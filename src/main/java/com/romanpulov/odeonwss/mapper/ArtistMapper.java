c06c0beebc467c746f7a324d0dfc9a1aed08b2b6
MrZeRo2000 <MrZeRo2020@outlook.com>
2023-07-14 17:32:34 +0300

    iss#141 Refactor ArtistRepository and service to work with DTO, cleanup unneeded: minor change

--- src/main/java/com/romanpulov/odeonwss/mapper/ArtistMapper.java
+++ src/main/java/com/romanpulov/odeonwss/mapper/ArtistMapper.java
@@ -3,8 +3,12 @@ package com.romanpulov.odeonwss.mapper;
 import com.romanpulov.odeonwss.dto.ArtistCategoriesDetailDTO;
 import com.romanpulov.odeonwss.dto.ArtistDTO;
 import com.romanpulov.odeonwss.entity.Artist;
+import com.romanpulov.odeonwss.entity.ArtistCategory;
 import com.romanpulov.odeonwss.entity.ArtistType;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ArtistMapper implements EntityDTOMapper<Artist, ArtistDTO> {
@@ -15,22 +19,23 @@ public class ArtistMapper implements EntityDTOMapper<Artist, ArtistDTO> {
 
     @Override
     public Artist fromDTO(ArtistDTO dto) {
-        Artist artist = new Artist();
-        artist.setType(
-                dto.getArtistType() == null ?
-                        ArtistType.ARTIST :
-                        dto.getArtistType());
-        artist.setName(dto.getArtistName());
+        Artist entity = new Artist();
 
-        return artist;
+        // immutable fields
+        entity.setId(dto.getId());
+
+        // mutable fields
+        this.update(entity, dto);
+
+        return entity;
     }
 
     @Override
     public void update(Artist entity, ArtistDTO dto) {
-        entity.setName(dto.getArtistName());
         if (dto.getArtistType() != null) {
             entity.setType(dto.getArtistType());
         }
+        entity.setName(dto.getArtistName());
     }
 
     public Artist fromDTO(ArtistCategoriesDetailDTO dto) {
