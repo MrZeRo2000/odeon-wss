0f23d015af44cc82d816a25d2a5eece6f4c92c3a
MrZeRo2000 <MrZeRo2000@mail.ru>
2024-04-21 22:39:20 +0300

    Script for avs chapters generation: validation for sample markers

--- scripts/py/src/generate_avs_chapter.py
+++ scripts/py/src/generate_avs_chapter.py
@@ -8,12 +8,22 @@ logger = get_logger('generate_avs_chapter')
 
 
 class AVSChapterGenerator:
+    AVS_CONTENT_START_MARKER = '{start}'
+    AVS_CONTENT_END_MARKER = '{end}'
+    RE_FRAME_START_MARKER = r"<startFrame>(\d+)"
+    RE_FRAME_END_MARKER = r"<endFrame>(\d+)"
+
     def __init__(self, sample_file_name):
         self.sample_file_name = os.path.abspath(sample_file_name)
         with open(self.sample_file_name, "r") as f:
             self.sample_content = f.read()
 
         self.path = os.path.dirname(sample_file_name)
+
+        if AVSChapterGenerator.AVS_CONTENT_START_MARKER not in self.sample_content:
+            raise ValueError(f'Start marker {AVSChapterGenerator.AVS_CONTENT_START_MARKER} not found in sample content')
+        if AVSChapterGenerator.AVS_CONTENT_END_MARKER not in self.sample_content:
+            raise ValueError(f'End marker {AVSChapterGenerator.AVS_CONTENT_END_MARKER} not found in sample content')
 
     def execute(self):
         for file_name in (p for p in os.listdir(self.path) if p.lower().endswith('clt')):
@@ -27,11 +37,13 @@ class AVSChapterGenerator:
         with open(os.path.join(self.path, file_name), "r") as f:
             content = f.read()
 
-        start_frame = re.search(r"<startFrame>(\d+)", content).group(1)
-        end_frame = re.search(r"<endFrame>(\d+)", content).group(1)
+        start_frame = re.search(AVSChapterGenerator.RE_FRAME_START_MARKER, content).group(1)
+        end_frame = re.search(AVSChapterGenerator.RE_FRAME_END_MARKER, content).group(1)
         logger.info(f'Start frame: {start_frame}, End frame: {end_frame}')
 
-        avs_content = self.sample_content.replace('{start}', start_frame).replace('{end}', end_frame)
+        avs_content = (self.sample_content
+                       .replace(AVSChapterGenerator.AVS_CONTENT_START_MARKER, start_frame)
+                       .replace(AVSChapterGenerator.AVS_CONTENT_END_MARKER, end_frame))
 
         with open(os.path.join(self.path, avs_file_name), "w") as f:
             f.write(avs_content)
