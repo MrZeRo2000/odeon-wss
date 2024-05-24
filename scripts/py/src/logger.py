9c0512c243fff27d7d79957e584b3f46fd30a3e8
MrZeRo2000 <MrZeRo2000@mail.ru>
2024-05-24 19:33:48 +0300

    Fixed logger encoding

--- scripts/py/src/logger.py
+++ scripts/py/src/logger.py
@@ -23,7 +23,7 @@ def get_logger(logger_name: str) -> logging.Logger:
     if os.path.exists(log_file_name):
         os.remove(log_file_name)
 
-    fh = logging.FileHandler(log_file_name)
+    fh = logging.FileHandler(log_file_name, encoding='utf-8')
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(formatter)
 
