1e52766a17f469b199cf8a611822575b0877f2f5
MrZeRo2000 <MrZeRo2000@mail.ru>
2024-06-28 23:25:27 +0300

    iss#225 Service, RestAPI: method to add MediaFiles as list of file names for an artifact: insert media files initial implementation and tests

--- src/main/java/com/romanpulov/odeonwss/service/MediaFileService.java
+++ src/main/java/com/romanpulov/odeonwss/service/MediaFileService.java
@@ -14,16 +14,15 @@ import com.romanpulov.odeonwss.service.processor.ProcessorException;
 import com.romanpulov.odeonwss.service.processor.parser.MediaParser;
 import com.romanpulov.odeonwss.service.processor.parser.NamesParser;
 import com.romanpulov.odeonwss.utils.media.MediaFileInfoException;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class MediaFileService
@@ -117,18 +116,52 @@ public class MediaFileService
                 () -> new CommonEntityNotFoundException("Artifact", artifactId)
         );
 
-        Path rootPath = Paths.get(artifactTypeService.getArtifactTypePath(artifact.getArtifactTypeId()));
-        Path filePath = Path.of(rootPath.toAbsolutePath().toString(), artifact.getTitle(), mediaFileName);
+        return mediaFileMapper.toDTO(getMediaFileAttributesByArtifact(
+                artifact.getArtifactTypeId(), artifact.getTitle(), mediaFileName));
+    }
+
+    private MediaFile getMediaFileAttributesByArtifact(long artifactTypeId, String artifactTitle, String mediaFileName)
+            throws WrongParameterValueException {
+        Path rootPath = Paths.get(artifactTypeService.getArtifactTypePath(artifactTypeId));
+        Path filePath = Path.of(rootPath.toAbsolutePath().toString(), artifactTitle, mediaFileName);
         if (Files.exists(filePath)) {
             try {
-                MediaFile mediaFile = mediaFileMapper.fromMediaFileInfo(mediaParser.parseTrack(filePath));
-                return mediaFileMapper.toDTO(mediaFile);
-
+                return mediaFileMapper.fromMediaFileInfo(mediaParser.parseTrack(filePath));
             } catch (MediaFileInfoException e)  {
                 throw new WrongParameterValueException("MediaFileName: parsing error", e.getMessage());
             }
         } else {
             throw new WrongParameterValueException("MediaFileName", mediaFileName);
         }
+    }
+
+    @Transactional
+    public RowsAffectedDTO insertMediaFiles(Long artifactId, List<String> mediaFileNames)
+            throws CommonEntityNotFoundException, WrongParameterValueException {
+        Artifact artifact = artifactRepository.findById(artifactId).orElseThrow(
+                () -> new CommonEntityNotFoundException("Artifact", artifactId)
+        );
+
+        Set<String> existingMediaFiles = getTableIdNameDuration(artifactId)
+                .stream()
+                .map(MediaFileDTO::getName)
+                .collect(Collectors.toUnmodifiableSet());
+
+        List<MediaFile> newMediaFiles = new ArrayList<>();
+
+        for (String mediaFileName: mediaFileNames) {
+            if (existingMediaFiles.contains(mediaFileName)) {
+                throw new WrongParameterValueException("mediaFileNames",
+                        "MediaFile " + mediaFileName + " already exists");
+            } else {
+                MediaFile mediaFile = this.getMediaFileAttributesByArtifact(
+                        artifact.getArtifactType().getId(), artifact.getTitle(), mediaFileName);
+                mediaFile.setArtifact(artifact);
+                newMediaFiles.add(mediaFile);
+            }
+        }
+
+        long savedCount = StreamSupport.stream(repository.saveAll(newMediaFiles).spliterator(), false).count();
+        return RowsAffectedDTO.from(savedCount);
     }
 }
--- src/test/java/com/romanpulov/odeonwss/service/ServiceMediaFileTest.java
+++ src/test/java/com/romanpulov/odeonwss/service/ServiceMediaFileTest.java
@@ -1,18 +1,165 @@
 package com.romanpulov.odeonwss.service;
 
+import com.romanpulov.odeonwss.builder.entitybuilder.EntityArtifactBuilder;
+import com.romanpulov.odeonwss.config.AppConfiguration;
+import com.romanpulov.odeonwss.entity.MediaFile;
+import com.romanpulov.odeonwss.generator.FileTreeGenerator;
 import com.romanpulov.odeonwss.repository.ArtifactRepository;
+import com.romanpulov.odeonwss.repository.ArtifactTypeRepository;
 import com.romanpulov.odeonwss.repository.MediaFileRepository;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.StreamSupport;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ServiceMediaFileTest {
+    private static final Logger log = Logger.getLogger(ServiceMediaFileTest.class.getSimpleName());
+
+    @Value("${test.data.path}")
+    String testDataPath;
+
+    @Autowired
+    private ArtifactTypeRepository artifactTypeRepository;
+
     @Autowired
     private ArtifactRepository artifactRepository;
 
     @Autowired
     private MediaFileRepository mediaFileRepository;
+
+    @Autowired
+    private MediaFileService service;
+
+    @Autowired
+    public ArtifactTypeService artifactTypeService;
+
+    private enum TestFolder {
+        TF_ONE_VIDEO_FILE
+    }
+
+    static class TestArtifactTypeService extends ArtifactTypeService {
+        private String artifactTypePath;
+
+        public TestArtifactTypeService(AppConfiguration appConfiguration, ArtifactTypeRepository artifactTypeRepository) {
+            super(appConfiguration, artifactTypeRepository);
+        }
+
+        public String getArtifactTypePath(long artifactTypeId) {
+            return artifactTypePath;
+        }
+
+        public void setArtifactTypePath(String artifactTypePath) {
+            this.artifactTypePath = artifactTypePath;
+        }
+    }
+
+    @TestConfiguration
+    static class ArtifactTypeServiceConfig {
+        @Bean
+        @Primary
+        ArtifactTypeService getArtifactTypeService(AppConfiguration appConfiguration, ArtifactTypeRepository artifactTypeRepository) {
+            return new TestArtifactTypeService(appConfiguration, artifactTypeRepository);
+        }
+    }
+
+    private Map<TestFolder, Path> tempDirs;
+
+    @BeforeAll
+    public void setup() throws Exception {
+        log.info("Before all");
+
+        tempDirs = FileTreeGenerator.createTempFolders(TestFolder.class);
+
+        FileTreeGenerator.generateFromJSON(
+                tempDirs.get(TestFolder.TF_ONE_VIDEO_FILE),
+                this.testDataPath,
+                """
+                            {
+                                "Scary Movie": {
+                                    "Scary Movie Part 1.mkv": "files/sample_1280x720_with_chapters.mkv",
+                                    "Scary Movie Part 2.mkv": "files/sample_1280x720_with_chapters.mkv",
+                                    "Scary Movie Part 3.mkv": "files/sample_1280x720_with_chapters.mkv"
+                                }
+                            }
+                        """
+        );
+
+        if (artifactTypeService instanceof TestArtifactTypeService) {
+            ((TestArtifactTypeService) artifactTypeService).setArtifactTypePath(
+                    tempDirs.get(TestFolder.TF_ONE_VIDEO_FILE).toString());
+        }
+
+    }
+
+    @AfterAll
+    public void teardown() {
+        log.info("After all");
+        FileTreeGenerator.deleteTempFiles(tempDirs.values());
+    }
+
+    @Test
+    @Sql({"/schema.sql", "/data.sql"})
+    @Order(1)
+    void testPrepare() {
+        artifactRepository.save(
+                new EntityArtifactBuilder()
+                        .withArtifactType(artifactTypeRepository.getWithDVMovies())
+                        .withTitle("Scary Movie")
+                        .build()
+        );
+
+        assertThat(artifactRepository.findById(1L)).isPresent();
+        assertThat(artifactRepository.findAll().size()).isEqualTo(1);
+    }
+
+    @Test
+    @Order(2)
+    void testGetMediaFileAttributes() throws Exception {
+        var dto = service.getMediaFileAttributes(1L, "Scary Movie Part 1.mkv");
+        assertThat(dto).isNotNull();
+        assertThat(dto.getName()).isEqualTo("Scary Movie Part 1.mkv");
+        assertThat(dto.getSize()).isNotNull();
+        assertThat(dto.getBitrate()).isNotNull();
+        assertThat(dto.getDuration()).isNotNull();
+        assertThat(dto.getFormat()).isEqualTo("MKV");
+        assertThat(dto.getWidth()).isEqualTo(1280L);
+        assertThat(dto.getHeight()).isEqualTo(720L);
+    }
+
+    @Test
+    @Order(3)
+    void testInsertToEmptyMediaFiles() throws Exception {
+        assertThat(StreamSupport
+                        .stream(mediaFileRepository
+                        .findAll().spliterator(), false)
+                        .count()
+        ).isEqualTo(0);
+
+        assertThat(service
+                        .insertMediaFiles(1L, List.of("Scary Movie Part 1.mkv", "Scary Movie Part 2.mkv"))
+                        .getRowsAffected()
+        ).isEqualTo(2L);
+
+        var mediaFiles = StreamSupport
+                .stream(mediaFileRepository.findAll().spliterator(), false)
+                .sorted(Comparator.comparing(MediaFile::getName))
+                .toList();
+        assertThat(mediaFiles).hasSize(2);
+    }
 }
