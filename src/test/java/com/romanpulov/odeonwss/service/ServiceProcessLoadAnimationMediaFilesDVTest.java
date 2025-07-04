c680bd3ccc97779ee5de93d9fc0dfe10909470cc
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-07-04 17:00:51 +0300

    iss#240 Get rid of separate test-data folder: ServiceProcessLoadAnimationMediaFilesDVTest migration

--- src/test/java/com/romanpulov/odeonwss/service/ServiceProcessLoadAnimationMediaFilesDVTest.java
+++ src/test/java/com/romanpulov/odeonwss/service/ServiceProcessLoadAnimationMediaFilesDVTest.java
@@ -2,24 +2,64 @@ package com.romanpulov.odeonwss.service;
 
 import com.romanpulov.odeonwss.entity.ArtifactType;
 import com.romanpulov.odeonwss.entity.MediaFile;
+import com.romanpulov.odeonwss.generator.FileTreeGenerator;
 import com.romanpulov.odeonwss.repository.ArtifactTypeRepository;
 import com.romanpulov.odeonwss.repository.MediaFileRepository;
 import com.romanpulov.odeonwss.service.processor.model.ProcessingStatus;
 import com.romanpulov.odeonwss.service.processor.model.ProcessorType;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.nio.file.Path;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ServiceProcessLoadAnimationMediaFilesDVTest {
     private static final Logger log = Logger.getLogger(ServiceProcessLoadAnimationMediaFilesDVTest.class.getSimpleName());
+
+    @Value("${test.data.path}")
+    String testDataPath;
+
+    private enum TestFolder {
+        TF_SERVICE_PROCESS_LOAD_ANIMATION_MEDIA_FILES_DV_TEST_OK
+    }
+
+    private Map<TestFolder, Path> tempFolders;
+
+    @BeforeAll
+    public void setup() throws Exception {
+        this.tempFolders = FileTreeGenerator.createTempFolders(TestFolder.class);
+
+        FileTreeGenerator.generateFromJSON(
+                tempFolders.get(TestFolder.TF_SERVICE_PROCESS_LOAD_ANIMATION_MEDIA_FILES_DV_TEST_OK),
+                this.testDataPath,
+                """
+                        {
+                          "Talespin": {
+                            "01 Plunder and Lightning (part-1).avi": "sample_AVI_480_750kB.avi",
+                            "01 Plunder and Lightning (part-2).avi": "sample_AVI_480_750kB.avi",
+                            "02 From Here to Machinery original.avi": "sample_AVI_480_750kB.avi",
+                            "03 Louie\\u2019s Last Stand.avi": "sample_AVI_480_750kB.avi"
+                          }
+                        }
+                     """
+        );
+    }
+
+    @AfterAll
+    public void teardown() {
+        log.info("After all");
+        FileTreeGenerator.deleteTempFiles(tempFolders.values());
+    }
 
     @Autowired
     ProcessService service;
@@ -42,7 +82,9 @@ public class ServiceProcessLoadAnimationMediaFilesDVTest {
     @Sql({"/schema.sql", "/data.sql"})
     @Rollback(false)
     void testPrepare() {
-        service.executeProcessor(ProcessorType.DV_ANIMATION_LOADER);
+        service.executeProcessor(
+                ProcessorType.DV_ANIMATION_LOADER,
+                tempFolders.get(TestFolder.TF_SERVICE_PROCESS_LOAD_ANIMATION_MEDIA_FILES_DV_TEST_OK).toString());
         assertThat(service.getProcessInfo().getProcessingStatus()).isEqualTo(ProcessingStatus.SUCCESS);
         log.info("Animation loader Processing info: " + service.getProcessInfo());
     }
@@ -52,7 +94,7 @@ public class ServiceProcessLoadAnimationMediaFilesDVTest {
     @Rollback(false)
     void testLoadMediaFiles() {
         // set size = 0
-        MediaFile mediaFile = mediaFileRepository.getMediaFilesByArtifactType(artifactType).get(0);
+        MediaFile mediaFile = mediaFileRepository.getMediaFilesByArtifactType(artifactType).getFirst();
         mediaFile.setSize(0L);
         mediaFileRepository.save(mediaFile);
 
@@ -60,7 +102,10 @@ public class ServiceProcessLoadAnimationMediaFilesDVTest {
                 artifactType).size();
         assertThat(oldCount).isGreaterThan(0);
 
-        service.executeProcessor(ProcessorType.DV_ANIMATION_MEDIA_LOADER);
+        service.executeProcessor(
+                ProcessorType.DV_ANIMATION_MEDIA_LOADER,
+                tempFolders.get(TestFolder.TF_SERVICE_PROCESS_LOAD_ANIMATION_MEDIA_FILES_DV_TEST_OK).toString());
+
         assertThat(service.getProcessInfo().getProcessingStatus()).isEqualTo(ProcessingStatus.SUCCESS);
         log.info("Animation Media Loader Processing info: " + service.getProcessInfo());
 
