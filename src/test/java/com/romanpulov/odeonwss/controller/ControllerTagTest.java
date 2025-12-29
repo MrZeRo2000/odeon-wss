2ba34dc3c84e790d5e57a0e28364e85c8f0f62a1
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-12-29 17:36:32 +0200

    Migration to Gradle 4.0 step 1: resolved compilation errors

--- src/test/java/com/romanpulov/odeonwss/controller/ControllerTagTest.java
+++ src/test/java/com/romanpulov/odeonwss/controller/ControllerTagTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
